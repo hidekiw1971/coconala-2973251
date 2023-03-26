cab7214f631e2761a61e2a6b52e01cc208806959
hideki watabe <hideki.w.1971@gmail.com>
2023-03-26 19:47:45 +0900

    js drawer button修正前

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   $(".js-drawer").click(function () {
     $(".js-drawer").toggleClass("js-drawer-open");
     $(".js-drawer-menu").toggleClass("js-drawer-menu-open");
-    $("body").toggleClass("active");
   });
 
   // 里親募集
@@ -62,6 +61,10 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         spaceBetween: 10,
       },
     },
+    navigation: {
+      nextEl: '.swiper-button-next', // 「次へ」ボタン要素のクラス
+      prevEl: '.swiper-button-prev', // 「前へ」ボタン要素のクラス
+    },
     on: { // イベントを登録する
       slideChange: (swiper) => {
         console.log('Slide index changed to: ' + (swiper.realIndex + 1));
@@ -78,7 +81,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $('.nekonote-q').not($(this)).removeClass('active');
   });
 
-
   // 
   // ここまで
 
@@ -86,50 +88,5 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
 
-  var topBtn = $('.pagetop');
-  topBtn.hide();
-
-  // ボタンの表示設定
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
-      topBtn.fadeIn();
-    } else {
-      // 画面が指定pxより上ならボタンを非表示
-      topBtn.fadeOut();
-    }
-  });
-
-  // ボタンをクリックしたらスクロールして上に戻る
-  topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
-    return false;
-  });
-
-  //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-
-  });
-
-
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
-  $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
-  });
 
 });
